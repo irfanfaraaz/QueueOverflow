f9beb7f909f8ea3db7d037cca70f6eb24454c312
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-09-26 20:21:45 +0530

    Navbar

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -84,7 +84,7 @@ const MobileNav = () => (
                     src="/assets/images/site-logo.svg"
                     width={23}
                     height={23}
-                    alt="Dev Overflow Logo"
+                    alt="Logo"
                 />
 
                 <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
