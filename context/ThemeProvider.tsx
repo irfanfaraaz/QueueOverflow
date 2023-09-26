0597acf93dc9eadb0aff743b7220130e64833567
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-09-26 19:42:20 +0530

    added Navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,17 +12,21 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
     const [mode, setMode] = useState("light");
     const handleThemeChange = () => {
-        if (mode === "dark") {
-            setMode("light");
-            document.documentElement.classList.add("light");
-        } else {
+        if (
+            localStorage.theme === "dark" ||
+            (!("theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
             setMode("dark");
             document.documentElement.classList.add("dark");
+        } else {
+            setMode("light");
+            document.documentElement.classList.remove("dark");
         }
     };
-    // useEffect(() => {
-    //     handleThemeChange();
-    // }, [mode]);
+    useEffect(() => {
+        handleThemeChange();
+    }, [mode]);
 
     return (
         <ThemeContext.Provider value={{ mode, setMode }}>
