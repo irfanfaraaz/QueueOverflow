4d6ff4abe79aa54e29ec0b29c2be754786738e75
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-09-30 00:04:27 +0530

    webhooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
