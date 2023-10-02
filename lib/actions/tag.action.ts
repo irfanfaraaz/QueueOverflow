d02c56a37794f1ecec97c102f813500211330d27
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-10-02 15:28:32 +0530

    feat: Tags section

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     try {
@@ -17,6 +18,17 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
             { _id: "2", name: "tag2" },
             { _id: "3", name: "tag3" },
         ];
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+    try {
+        connectToDatabase();
+        const tags = await Tag.find({});
+        return { tags };
     } catch (e) {
         console.log(e);
         throw e;
