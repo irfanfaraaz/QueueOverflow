c5930f545d9336d2ec502265405db3d0c8956d32
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-12-16 22:24:18 +0530

    feat: nextjs 14

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,6 +10,12 @@ import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Home | QueueOverflow",
+    description: "QueueOverflow is a Q&A platform for developers",
+};
 export default async function Home({ searchParams }: SearchParamsProps) {
     const result = await getQuestions({
         searchQuery: searchParams.q,
